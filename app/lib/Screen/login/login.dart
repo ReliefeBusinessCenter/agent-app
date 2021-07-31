bf4faa61dd7b188ebc0acb49c3af2eee822c7507
yared123yared <yaredyaya16@gmail.com>
2021-07-31 01:18:42 -0700

    updated some changes on the login

--- app/lib/Screen/login/login.dart
+++ app/lib/Screen/login/login.dart
@@ -1,3 +1,4 @@
+import 'package:app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -15,16 +16,18 @@ class Login extends StatelessWidget {
             Container(
               padding: EdgeInsets.only(top: 55),
               width: double.infinity,
-              color: Theme.of(context).primaryColor,
+              // color: Theme.of(context).primaryColor,
+              color: Colors.white,
               child: Column(
                 children: [
                   Text(
-                    'Welcome to csv',
+                    'Welcome to Trust Brokers',
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                        fontWeight: FontWeight.w300,
-                        fontSize: displayHeight(context) * 0.04,
-                        color: Colors.white),
+                      fontWeight: FontWeight.w300,
+                      fontSize: displayHeight(context) * 0.04,
+                      color: Theme.of(context).primaryColor,
+                    ),
                   ),
                   SizedBox(
                     height: height * 0.02,
@@ -32,7 +35,7 @@ class Login extends StatelessWidget {
                   Container(
                     // color: Colors.transparent,
                     child: new Image.asset(
-                      'assets/images/login/csv_img.png',
+                      'assets/images/login/logo.png',
                       height: MediaQuery.of(context).size.height * 0.25,
                       width: MediaQuery.of(context).size.width * 0.65,
                       fit: BoxFit.fill,
@@ -61,65 +64,123 @@ class Login extends StatelessWidget {
               height: height * 0.06,
             ),
             Material(
-              elevation: 6.0,
+              color: Colors.white,
+              elevation: 1.0,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(40)),
-              child: TextField(
-                // controller: _myPasswordController,
-                // obscureText: !ispassshow,
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-                onEditingComplete: () => FocusScope.of(context).unfocus(),
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(top: 14),
-                  prefixIcon: Icon(Icons.lock),
-                  border: InputBorder.none,
-                  // suffixIcon: IconButton(
-                  //   icon: Icon(ispassshow
-                  //       ? Icons.visibility
-                  //       : Icons.visibility_off),
-                  //   onPressed: () {
-                  //     setState(() {
-                  //       ispassshow = !ispassshow;
-                  //     });
-                  //   },
-                  // ),
-                  hintText: "Enter Email ",
+              child: Container(
+                width: width * 0.9,
+                child: TextField(
+                  // controller: _myPasswordController,
+                  // obscureText: !ispassshow,
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.done,
+                  onEditingComplete: () => FocusScope.of(context).unfocus(),
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top: 14),
+                    prefixIcon: Icon(Icons.email),
+                    border: InputBorder.none,
+                    // suffixIcon: IconButton(
+                    //   icon: Icon(ispassshow
+                    //       ? Icons.visibility
+                    //       : Icons.visibility_off),
+                    //   onPressed: () {
+                    //     setState(() {
+                    //       ispassshow = !ispassshow;
+                    //     });
+                    //   },
+                    // ),
+                    hintText: "Email Address",
+                  ),
                 ),
               ),
             ),
             SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
+                // width: MediaQuery.of(context).size.width * 0.5,
                 height: MediaQuery.of(context).size.height * 0.01),
             Material(
               color: Colors.white,
-              elevation: 6.0,
+              elevation: 1.0,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(40)),
-              child: TextField(
-                // controller: _myPasswordController,
-                // obscureText: !ispassshow,
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-                onEditingComplete: () => FocusScope.of(context).unfocus(),
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(top: 14),
-                  prefixIcon: Icon(Icons.lock),
-                  border: InputBorder.none,
-                  // suffixIcon: IconButton(
-                  //   icon: Icon(ispassshow
-                  //       ? Icons.visibility
-                  //       : Icons.visibility_off),
-                  //   onPressed: () {
-                  //     setState(() {
-                  //       ispassshow = !ispassshow;
-                  //     });
-                  //   },
-                  // ),
-                  hintText: "Enter Password ",
+              child: Container(
+                width: width * 0.9,
+                child: TextField(
+                  // controller: _myPasswordController,
+                  // obscureText: !ispassshow,
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.done,
+                  onEditingComplete: () => FocusScope.of(context).unfocus(),
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top: 14),
+                    prefixIcon: Icon(Icons.lock),
+                    border: InputBorder.none,
+                    // suffixIcon: IconButton(
+                    //   icon: Icon(ispassshow
+                    //       ? Icons.visibility
+                    //       : Icons.visibility_off),
+                    //   onPressed: () {
+                    //     setState(() {
+                    //       ispassshow = !ispassshow;
+                    //     });
+                    //   },
+                    // ),
+                    hintText: "Enter Password ",
+                  ),
                 ),
+              ),
+            ),
+            SizedBox(
+              height: height * 0.06,
+            ),
+            SizedBox(
+                //width: MediaQuery.of(context).size.width*0.5,
+                height: MediaQuery.of(context).size.height * 0.06,
+                child: InkWell(
+                  splashColor: Colors.white,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  },
+
+                  // onTap: () => _pushPage(context, Register()),
+                  child: Material(
+                    color: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)),
+                    elevation: 1.0,
+                    child: Container(
+                      // padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40)),
+                      height: 100,
+                      width: MediaQuery.of(context).size.width * 0.75,
+                      child: Center(
+                          child: Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w300),
+                      )),
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: height * 0.03,
+            ),
+            GestureDetector(
+              child: Text(
+                'Forgot Passowrd?',
+                // textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: height * 0.025,
+                    color: Colors.black),
               ),
             ),
           ],
