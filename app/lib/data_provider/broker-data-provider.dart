e6a3b4f3543b8b77e32135291b65b90c3aea6c97
Solomon <se.solomon.kasahun@gmail.com>
2022-11-08 16:32:18 +0300

    late inititization in map screen fixed

--- app/lib/data_provider/broker-data-provider.dart
+++ app/lib/data_provider/broker-data-provider.dart
@@ -3,28 +3,23 @@ import 'package:app/Service/fireabse_service.dart';
 import 'package:app/ip/ip.dart';
 import 'package:app/model/broker/broker.dart';
 import 'package:app/preferences/user_preference_data.dart';
-
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
-
 class BrokerDataProvider {
   final _baseUrl = '${Ip.ip}/api/brokers';
   final http.Client httpClient;
   final UserPreferences userPreferences;
   late String? token;
   BrokerDataProvider({required this.httpClient, required this.userPreferences});
-
   Future<void> initState() async {
     token = await userPreferences.getUserToken();
   }
-
 // get broker data
   Future<List<Broker>?> getBrokers() async {
     print("I am on get broker method: ");
     initState();
     try {
       final url = Uri.parse('$_baseUrl');
-
       final response = await http.get(
         url,
         headers: {
@@ -45,7 +40,6 @@ class BrokerDataProvider {
       throw Exception(e);
     }
   }
-
 // get broker by phone
   Future<Broker?> getBrokerByPhone(String phone) async {
     initState();
@@ -71,7 +65,6 @@ class BrokerDataProvider {
     }
     return null;
   }
-
 // get broker by Id
   Future<Broker?> getBrokerById(int id) async {
     initState();
@@ -85,7 +78,6 @@ class BrokerDataProvider {
           'Authorization': 'Bearer $token',
         },
       );
-
       if (response.statusCode == 200) {
         final extractedData = json.decode(response.body);
         final data = extractedData;
@@ -98,25 +90,10 @@ class BrokerDataProvider {
     }
     // return broker_return;
   }
-
   Future<bool> createBroker(Broker? broker) async {
     initState();
     try {
       final url = Uri.parse('$_baseUrl');
-      // var request = http.MultipartRequest(
-      //     'POST', Uri.parse('${Ip.ip}/api/users/uploadfileg'));
-      // var request2 = http.MultipartRequest(
-      //     'POST', Uri.parse('${Ip.ip}/api/users/uploadfileg'));
-
-      // request.files.add(await http.MultipartFile.fromPath(
-      //     'file', broker?.user!.picture as String));
-
-      // var res = await http.Response.fromStream(await request.send());
-
-      // request2.files.add(await http.MultipartFile.fromPath(
-      //     'file', broker?.user!.identificationCard as String));
-
-      // var resId = await http.Response.fromStream(await request2.send());
       try {
         if (broker?.user!.picture == null)
           throw new Exception("Profile picture is required");
@@ -124,75 +101,82 @@ class BrokerDataProvider {
           throw new Exception("Broker Identification card is required");
         String? piPath;
         // String? iPath;
-        String picturePath =
-            await FirebaseService.uploadFile((broker!.user!.picture), "users/")
-                .then((value) => {piPath = value})
-                .toString();
+        await FirebaseService.uploadFile((broker!.user!.picture), "users/")
+            .then((picturePath) async {
+          print('Picture path: ${picturePath.toString()}');
+          await FirebaseService.uploadFile(
+                  broker.user!.identificationCard, "IdentificationCard/")
+              .then((idPath) async {
+            final response = await http.post(url,
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json',
+                  'Authorization': 'Bearer $token',
+                },
+                body: jsonEncode({
+                  "portfolios": [],
+                  "deliveries": [],
+                  "deals": [],
+                  "reviews": [],
+                  "skills": {
+                    "communicationSkill": broker.skills!.communicationSkill,
+                    "brokingSkill": broker.skills!.brokingSkill,
+                    "workDone": broker.skills!.workDone,
+                    "workInProgress": broker.skills!.workInProgress?.toDouble(),
+                    //  "about": broker.skills!.about.toString(),
+                  },
+                  "about": broker.skills!.about,
+                  "categoryId": 2,
+                  // "category": {
+                  //   // "categoryId"
+                  //   // "catigoryName": broker.category!.catigory
+                  //   // "categoryId": 2,
+                  //   "catigoryName": "Bussiness1  Broker"
+                  // },
+                  "user": {
+                    "fullName": broker.user!.fullName,
+                    "email": "Someone@gmail.com",
+                    "password": broker.user!.password,
+                    "phone": '0${broker.user!.phone!.substring(4)}',
+                    // "address": "Ethiopia/Dessie",
+                    "picture": picturePath.toString(),
+                    "city": broker.user!.city,
 
-        String idPath = FirebaseService.uploadFile(
-                broker.user!.identificationCard, "IdentificationCard/")
-            // .then((value) => {
-            //   iPath = value
-            // })
-            .toString();
+    
+          
+            
+    
 
-        print(
-            "c s  about i iss : ${broker.skills!.about} Identification path is :  ${idPath}");
-        final response = await http.post(url,
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-              'Authorization': 'Bearer $token',
-            },
-            body: jsonEncode({
-              "portfolios": [],
-              "deliveries": [],
-              "deals": [],
-              "reviews": [],
-              "skills": {
-                "communicationSkill": broker.skills!.communicationSkill,
-                "brokingSkill": broker.skills!.brokingSkill,
-                "workDone": broker.skills!.workDone,
-                "workInProgress": broker.skills!.workInProgress?.toDouble(),
-                //  "about": broker.skills!.about.toString(),
-              },
-              "about": broker.skills!.about,
-              "categoryId": 2,
-              
-              // "category": {
-              //   // "categoryId"
-              //   // "catigoryName": broker.category!.catigory
-              //   // "categoryId": 2,
-              //   "catigoryName": "Bussiness1  Broker"
-              // },
-
-              "user": {
-                "fullName": broker.user!.fullName,
-                "email": "Someone@gmail.com",
-                "password": broker.user!.password,
-                "phone": broker.user!.phone,
-                // "address": "Ethiopia/Dessie",
-                "picture": "dentificationCard/image_picker846259987.jpg",
-                "city": "broker.user!.city",
-                "subcity": broker.user!.subCity,
-                "kebele": broker.user!.kebele,
-                "sex": broker.user!.sex,
-                "identificationCard":
-                    "dentificationCard/image_picker846259987.jpg",
-                "role": broker.user!.role,
-                "buys": "",
-                "latitude": broker.user!.latitude,
-                "longtiude": broker.user!.longitude
-              }
-            }));
-        if (response.statusCode == 200) {
-          return true;
-        } else {
-          print(response.body);
-          print(
-              "Picture path: ${picturePath} Identification path:  ${idPath}   and about done is ");
-          throw Exception('Failed to load courses');
-        }
+          
+    
+    
+  
+                    "subcity": broker.user!.subCity,
+                    "kebele": broker.user!.kebele,
+                    "sex": broker.user!.sex,
+                    "identificationCard": idPath.toString(),
+                    "role": broker.user!.role,
+                    "buys": "",
+                    "latitude": broker.user!.latitude,
+                    "longtiude": broker.user!.longitude
+                  }
+                }));
+            if (response.statusCode == 200) {
+              return true;
+            } else {
+              print(response.body);
+              print(
+                  "Picture path: ${picturePath} Identification path:  ${idPath}   and about done is ");
+              throw Exception('Failed to load courses');
+            }
+          });
+        }).toString();
+        // String idPath = FirebaseService.uploadFile(
+        //         broker.user!.identificationCard, "IdentificationCard/")
+        //     // .then((value) => {
+        //     //   iPath = value
+        //     // })
+        //     .toString();
       } catch (e) {
         print("Error occured: $e");
       }
@@ -201,7 +185,6 @@ class BrokerDataProvider {
     }
     return false;
   }
-
 // delete Broker
   Future<bool> deleteBrokerEvent(int id) async {
     initState();
@@ -210,7 +193,6 @@ class BrokerDataProvider {
       final response = await http.delete(
         url,
       );
-
       if (response.statusCode == 200) {
         return true;
       } else {
@@ -221,7 +203,6 @@ class BrokerDataProvider {
     }
     return false;
   }
-
 // Update Broker
   Future<Broker> updateBroker(Broker broker, bool status) async {
     initState();
@@ -263,7 +244,6 @@ class BrokerDataProvider {
       throw Exception("Something went wrong");
     }
   }
-
 // update broker profile
   Future<Broker> updateBrokerProfile(Broker broker, bool imageChanged) async {
     initState();
@@ -305,10 +285,8 @@ class BrokerDataProvider {
       } else {
         var request = http.MultipartRequest(
             'POST', Uri.parse('${Ip.ip}/api/users/uploadfileg'));
-
         request.files.add(await http.MultipartFile.fromPath(
             'file', broker.user!.picture as String));
-
         var res = await http.Response.fromStream(await request.send());
         if (res.statusCode == 200) {
           final url = Uri.parse('${Ip.ip}/api/brokers/${broker.brokerId}');
@@ -353,7 +331,6 @@ class BrokerDataProvider {
               },
             ),
           );
-
           if (_response.statusCode == 200) {
             return Broker.fromJson(jsonDecode(_response.body));
           } else {
